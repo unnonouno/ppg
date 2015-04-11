11dab092746faf064dca84a52069a5caf248be8e
Yuya Unno <unnonouno@gmail.com>
2015-04-11 16:59:41 +0900

    Fix file format

--- src/util.hpp
+++ src/util.hpp
@@ -1,7 +1,7 @@
 #ifndef UTIL_HPP_E5A9DB5E_9EA5_4429_8D51_EA1321AF736F_
 #define UTIL_HPP_E5A9DB5E_9EA5_4429_8D51_EA1321AF736F_
 
-#include <iostream>
+#include <ostream>
 #include <string>
 #include <vector>
 
@@ -9,7 +9,6 @@
 
 #define STR(s) \
   (sstream() << s).str()
-
 
 #define FOREACH(it, col)                                                \
   for (typeof(col.begin()) it = col.begin(); it != col.end(); ++it)
@@ -22,18 +21,19 @@ namespace ppg {
 std::vector<char_t> str_to_read(const std::string&);
 std::string read_to_str(const read_t&);
 
-}
-
 template <typename T>
 std::ostream& operator << (std::ostream& out, const std::vector<T>& v) {
   out << "[";
   FOREACH(x, v) {
-    if (x != v.begin())
+    if (x != v.begin()) {
       out << ';';
+    }
     out << *x;
   }
   out << "]";
   return out;
 }
+
+}  // namespace ppg
 
 #endif  // UTIL_HPP_E5A9DB5E_9EA5_4429_8D51_EA1321AF736F_
