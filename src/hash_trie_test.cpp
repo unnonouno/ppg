ac2dc8180cae0c49df69aaae838380402d68d808
Yuya Unno <unnonouno@gmail.com>
2015-04-11 15:49:02 +0900

    Fix test for unordered map

--- src/hash_trie_test.cpp
+++ src/hash_trie_test.cpp
@@ -18,6 +18,7 @@ TEST(hash_trie, trivial) {
   read_t abd = str_to_read("ABD");
 
   t.insert(abc, "ABC", 1);
+  // [abc]
   t.print(cout);
 
   read_t r;
@@ -27,7 +28,7 @@ TEST(hash_trie, trivial) {
   EXPECT_EQ(abc, r);
 
   t.insert(abc, "ABC", 1);
-
+  // [abc, abc]
   t.get_ith(ab, 0, w, r);
   EXPECT_EQ("ABC", w);
   EXPECT_EQ(abc, r);
@@ -36,15 +37,19 @@ TEST(hash_trie, trivial) {
   EXPECT_EQ(abc, r);
 
   t.insert(abd, "ABD", 1);
-  t.get_ith(ab, 0, w, r);
-  EXPECT_EQ("ABC", w);
-  EXPECT_EQ(abc, r);
-  t.get_ith(ab, 1, w, r);
-  EXPECT_EQ("ABC", w);
-  EXPECT_EQ(abc, r);
-  t.get_ith(ab, 2, w, r);
-  EXPECT_EQ("ABD", w);
-  EXPECT_EQ(abd, r);
+  // [abc, abc, abd]
+  // Order for C and D is not defined
+  read_t r1, r2, r3;
+  string w1, w2, w3;
+  t.get_ith(ab, 0, w1, r1);
+  t.get_ith(ab, 1, w2, r2);
+  t.get_ith(ab, 2, w3, r3);
+  EXPECT_TRUE(
+      (w1 == "ABC" && w2 == "ABC" && w3 == "ABD" &&
+       r1 == abc && r2 == abc && r3 == abd) ||
+      (w1 == "ABD" && w2 == "ABC" && w3 == "ABC" &&
+       r1 == abd && r2 == abc && r3 == abc));
+
   t.print(cout);
   EXPECT_EQ(3u, t.count_total(ab));
   EXPECT_EQ(3u, t.count_total(a));
@@ -52,22 +57,21 @@ TEST(hash_trie, trivial) {
 
 
   t.insert(a, "A", 1);
+  // [a, abc, abc, abd]
   t.print(cout);
   t.get_ith(a, 0, w, r);
   EXPECT_EQ("A", w);
   EXPECT_EQ(a, r);
 
-  t.get_ith(a, 1, w, r);
-  EXPECT_EQ("ABC", w);
-  EXPECT_EQ(abc, r);
+  t.get_ith(a, 1, w1, r1);
+  t.get_ith(a, 2, w2, r2);
+  t.get_ith(a, 3, w3, r3);
 
-  t.get_ith(a, 2, w, r);
-  EXPECT_EQ("ABC", w);
-  EXPECT_EQ(abc, r);
-
-  t.get_ith(a, 3, w, r);
-  EXPECT_EQ("ABD", w);
-  EXPECT_EQ(abd, r);
+  EXPECT_TRUE(
+      (w1 == "ABC" && w2 == "ABC" && w3 == "ABD" &&
+       r1 == abc && r2 == abc && r3 == abd) ||
+      (w1 == "ABD" && w2 == "ABC" && w3 == "ABC" &&
+       r1 == abd && r2 == abc && r3 == abc));
 }
 
 }
